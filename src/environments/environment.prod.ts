5601b14b171fa8790b56be86f5c03fb21fd58084
strale-bandzi <bandzi.fearless@gmail.com>
2018-06-25 00:32:41 +0200

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiKey: 'AIzaSyBYNVP4qhntgrkSV0Mg6FTwgPuPbAhhtvE',
+    authDomain: 'pdg-listing-dbase.firebaseapp.com',
+    databaseURL: 'https://pdg-listing-dbase.firebaseio.com',
+    projectId: 'pdg-listing-dbase',
+    storageBucket: 'pdg-listing-dbase.appspot.com',
+    messagingSenderId: '313072083209'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
 // Initialize Firebase
   config: {
     apiKey: 'AIzaSyBYNVP4qhntgrkSV0Mg6FTwgPuPbAhhtvE',
