b6d4dda365b89780eaf108854cc09c012429a94d
strale-bandzi <bandzi.fearless@gmail.com>
2018-06-24 23:55:36 +0200

    removed prod files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  config: {
-    apiKey: 'AIzaSyBYNVP4qhntgrkSV0Mg6FTwgPuPbAhhtvE',
-    authDomain: 'pdg-listing-dbase.firebaseapp.com',
-    databaseURL: 'https://pdg-listing-dbase.firebaseio.com',
-    projectId: 'pdg-listing-dbase',
-    storageBucket: 'pdg-listing-dbase.appspot.com',
-    messagingSenderId: '313072083209'
-  }
+  production: true
 };
